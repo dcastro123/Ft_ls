25c45c7f0115a89692a477bbbc64bef06e5fff28
David CASTRO-CHEW <dcastro-@e1z2r6p3.42.us.org>
2017-05-23 18:24:13 -0700

    first commit

---
+++ libft/get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcastro- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/02/16 22:35:16 by dcastro-          #+#    #+#             */
+/*   Updated: 2017/05/23 18:11:58 by dcastro-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include "libft.h"
+
+# define BUFF_SIZE 32
+
+int		get_next_line(int fd, char **line);
+#endif
