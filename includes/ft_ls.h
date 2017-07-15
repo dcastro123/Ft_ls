44220217ee3b30b6ecd0f48ac3876904a0310a3c
David CASTRO-CHEW <dcastro-@e1z2r6p3.42.us.org>
2017-07-14 21:46:21 -0700

    woohoo getting started

---
+++ includes/ft_ls.h
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ls.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcastro- <dcastro-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/14 20:59:18 by dcastro-          #+#    #+#             */
+/*   Updated: 2017/07/14 21:27:48 by dcastro-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LS_H
+# define FT_LS_H
+
+#include <stdio.h>
+#include "libft.h"
+#include <dirent.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/stat.h>
+#include <pwd.h>
+#include <uuid/uuid.h>
+#include <sys/types.h>
+#include <grp.h>
+#include <sys/xattr.h>
+
+typedef struct 		s_env
+{
+
+
+
+
+
+}					t_env;
+
+typedef struct 		s_data
+{
+
+
+
+
+
+
+}					t_data;
